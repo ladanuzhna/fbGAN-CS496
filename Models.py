6b0d8995431919cbbb6d0e920a7ff0addbaaf47e
ladanuzhna <ladanuzhna2022@u.northwestern.edu>
2020-11-01 10:28:40 -0600

    skeleton for classes

--- Models.py
+++ Models.py
@@ -3,36 +3,35 @@ import tensorflow as tf
 class Generator(tf.keras.Model):
 
     def __init__(self,input_size = 50):
+        """
+        implementation of Generator
+        :param input_size: size of the sequence (input noise)
+        """
         super().__init__(name='generator')
 
-    def optimizer(self):
-        pass
-
-    def train(self):
-        pass
-
-    def objective(self):
-        pass
-
-    def test(self):
-        pass
+    def __call__(self, X):
+        """
+        model's forward pass
+        :param X - input of the size [batch_size, seq_length]
+        :return: Z - batch of generated sequences
+        """
 
 
 class Discriminator(tf.keras.Model):
 
-    def __init__(self):
+    def __init__(self, clip = 1):
+        """
+        implementation of Discriminator
+        :param clip: value to which you clip the gradients (or False)
+        """
         super().__init__(name='discriminator')
 
-    def optimizer(self):
-        pass
-
-    def train(self):
-        pass
-
-    def objective(self):
-        pass
-
-    def test(self):
+    def call(self,X):
+        """
+        model's forward pass
+        :param X - input of the size [batch_size, seq_length]
+        :return: Y - probability of each sequences being real of shape [batch_size, 1]
+        """
         pass
 
 class Feedback(tf.keras.Model):
@@ -40,15 +39,6 @@ class Feedback(tf.keras.Model):
     def __init__(self):
         pass
 
-    def optimizer(self):
-        pass
-
-    def objective(self):
-        pass
-
-    def train(self):
-        pass
-
-    def test(self):
+    def __call__(self):
         pass
 
