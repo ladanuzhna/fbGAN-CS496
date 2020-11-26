68a3161e28e388a0b3d4625cc267e659265dbbfa
karinazad <karinazadorozhny2022@u.northwestern.edu>
2020-11-26 15:30:32 +0100

    protein to DNA conversion

---
+++ utils/protein_utilities.py
@@ -0,0 +1,44 @@
+import numpy
+import pandas
+from data_utilities import parse
+
+# Adapted from https://github.com/niranjangavade98/DNA-to-Protein-sequence
+
+DNA_protein_MAP = {
+            'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+            'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+            'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+            'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+            'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+            'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+            'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+            'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+            'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+            'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+            'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+            'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+            'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+            'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+            'TAC': 'Y', 'TAT': 'Y', 'TAA': 'P', 'TAG': 'P',
+            'TGC': 'C', 'TGT': 'C', 'TGA': 'P', 'TGG': 'W',
+        }
+
+protein_DNA_MAP = {v: k for k, v in DNA_to_protein.items()}
+protein_DNA_MAP['P'] = 'TAG'
+
+
+def protein_to_DNA(protein_sequences, triplets = False):
+    global protein_DNA_MAP
+    parsed = parse(protein_sequences)
+
+    DNA_sequences = []
+
+    for seq in parsed:
+        DNA = [protein_DNA_MAP[a] for a in seq]
+        DNA_sequences = ['ATG'  + "".join(DNA) + "TAG"]
+
+    return DNA_sequences
+
+
+
+
